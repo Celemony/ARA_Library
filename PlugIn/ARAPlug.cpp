2ca05a7562c2ed32ab290760f25d394dabeb5d9f
sgretscher <41306803+sgretscher@users.noreply.github.com>
2021-11-24 19:08:24 +0100

    Fix minor typos

--- PlugIn/ARAPlug.cpp
+++ PlugIn/ARAPlug.cpp
@@ -2733,7 +2733,7 @@ void PlugInEntry::initializeARAWithConfiguration (const ARAInterfaceConfiguratio
 
     const SizedStructPtr<ARAInterfaceConfiguration> ptr { config };
     ARA_VALIDATE_API_STRUCT_PTR (ptr, ARAInterfaceConfiguration);
-    ARA_VALIDATE_API_ARGUMENT (ptr, getFactory ()->lowestSupportedApiGeneration <= config->desiredApiGeneration);
+    ARA_VALIDATE_API_ARGUMENT (ptr, getFactory ()->lowestSupportedApiGeneration <= ptr->desiredApiGeneration);
     ARA_VALIDATE_API_ARGUMENT (ptr, ptr->desiredApiGeneration <= getFactory ()->highestSupportedApiGeneration);
 
     _usedApiGeneration = ptr->desiredApiGeneration;
