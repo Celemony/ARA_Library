ae9512c97c3a2b8da9d306ee208c10cf195172b9
sgretscher <41306803+sgretscher@users.noreply.github.com>
2025-01-10 16:53:32 +0100

    Bump copyright year to 2025

--- IPC/ARAIPCAudioUnit_v3.h
+++ IPC/ARAIPCAudioUnit_v3.h
@@ -2,7 +2,7 @@
 //! \file       ARAIPCAudioUnit_v3.h
 //!             Implementation of ARA IPC message sending through AUMessageChannel
 //! \project    ARA SDK Library
-//! \copyright  Copyright (c) 2021-2024, Celemony Software GmbH, All Rights Reserved.
+//! \copyright  Copyright (c) 2021-2025, Celemony Software GmbH, All Rights Reserved.
 //! \license    Licensed under the Apache License, Version 2.0 (the "License");
 //!             you may not use this file except in compliance with the License.
 //!             You may obtain a copy of the License at
